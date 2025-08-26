d3352d2a85fad3c1760c3deb977d5faaa1cef96b
Kubnong <riw3566@gmail.com>
2025-08-26 17:43:18 +0700

    [Add] middleware for admin dashboard page

---
+++ src/middleware.tsx
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse} from "next/server";
+import { createClient } from "@/lib/supabase/server";
+
+export async function middleware(request: NextRequest) {
+    const supabase = await createClient()
+
+    const {data: { user }, error} = await supabase.auth.getUser()
+    if(error || !user) {
+        return NextResponse.redirect(new URL('/login', request.url))
+    }
+    console.log(user)
+
+    const { data: adminData, error: adminError } = await supabase
+        .from('admins')
+        .select('*')
+        .eq('admin_id', user.id)
+    
+    if(adminError) {
+        console.log('query Error',adminError)
+        return NextResponse.redirect(new URL('/login', request.url))
+    }
+
+    if (!adminData || adminData.length === 0) {
+        return NextResponse.redirect(new URL('/', request.url))
+    }
+
+    return NextResponse.next()
+}
+
+export const config = {
+    matcher: ['/admin/:path*'] // match /admin และทุก path ต่อท้าย
+}
