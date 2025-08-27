56ac9ac37d3a1bbb5b78371fa14d285e727e135a
Kubnong <riw3566@gmail.com>
2025-08-28 02:50:28 +0700

    [Add] Admin Dashboard UI

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,47 +1,76 @@
-import { createClient } from "@/lib/supabase/server"
-import { redirect } from 'next/navigation'
+import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
+import { User } from "lucide-react";
+import { ClipboardList } from "lucide-react";
+import { MessageCircleMore } from "lucide-react";
 
 export default async function Page() {
-  const supabase = await createClient()
+  const supabase = await createClient();
 
-  const { data: users , error: getUsersError } = await supabase
-    .from('users')
-    .select('*')
+  const { data: users, error: getUsersError } = await supabase
+    .from("users")
+    .select("*");
 
-  if(getUsersError) {
-    console.log('Error',getUsersError)
-    redirect('/error')
+  if (getUsersError) {
+    console.log("Error", getUsersError);
+    redirect("/error");
   }
 
-  const { data: boards , error: getBoardsError } = await supabase
-    .from('board')
-    .select('*')
+  const { data: boards, error: getBoardsError } = await supabase
+    .from("board")
+    .select("*");
 
-  if(getBoardsError) {
-    console.log('Error',getBoardsError)
-    redirect('/error')
+  if (getBoardsError) {
+    console.log("Error", getBoardsError);
+    redirect("/error");
   }
 
-  const { data: comments , error: getCommentsError } = await supabase
-    .from('comment')
-    .select('*')
+  const { data: comments, error: getCommentsError } = await supabase
+    .from("comment")
+    .select("*");
 
-  if(getCommentsError) {
-    console.log('Error',getCommentsError)
-    redirect('/error')
+  if (getCommentsError) {
+    console.log("Error", getCommentsError);
+    redirect("/error");
   }
 
   return (
-    <div>
-      <div>
-        Total User : {users.length}
+    <div className="bg-black flex flex-col justify-center items-center h-screen gap-y-10">
+      <div className="font-bold text-4xl bg-gradient-to-r from-purple-500 via-blue-500 to-green-500 text-transparent bg-clip-text">
+        Dashboard Overview
       </div>
-      <div>
-        Total board : {boards.length}
-      </div>
-      <div>
-        Total comment : {comments.length}
+      <div className="flex gap-10">
+        {/* user */}
+        <div className="flex border-2 border-indigo-400 bg-purple-500 p-5 gap-5 items-center rounded-2xl">
+          <div className="flex flex-col">
+            <div className="font-semibold">Total User</div>
+            <div className="text-4xl font-bold">{users.length}</div>
+          </div>
+          <div>
+            <User size={40}/>
+          </div>
+        </div>
+        {/* board */}
+        <div className="flex border-2 border-indigo-400 bg-purple-500 p-5 gap-5 items-center rounded-2xl">
+          <div className="flex flex-col">
+            <div className="font-semibold">Total Board</div>
+            <div className="text-4xl font-bold">{boards.length}</div>
+          </div>
+          <div>
+            <ClipboardList size={40}/>
+          </div>
+        </div>
+        {/* comment */}
+        <div className="flex border-2 border-indigo-400 bg-purple-500 p-5 gap-5 items-center rounded-2xl">
+          <div className="flex flex-col">
+            <div className="font-semibold">Total Comment</div>
+            <div className="text-4xl font-bold">{comments.length}</div>
+          </div>
+          <div>
+            <MessageCircleMore size={40}/>
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Page() {
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-black gap-y-8">
-      <div className="text-5xl font-bold">
+      <div className="md:text-5xl text-2xl font-bold">
         ⚠️ Sorry, something went wrong
       </div>
       <Link href={'/'} className="p-3 border border-white rounded-2xl">
