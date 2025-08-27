15bbbe7c96f50d74ee95dd64c8fad9492f66ceee
Kubnong <riw3566@gmail.com>
2025-08-27 23:52:16 +0700

    [Add] Ui Error page

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -1,5 +1,14 @@
-'use client'
+import Link from "next/link";
 
 export default function Page() {
-  return <p>Sorry, something went wrong</p>
+  return (
+    <div className="flex flex-col justify-center items-center h-screen bg-black gap-y-8">
+      <div className="text-5xl font-bold">
+        ⚠️ Sorry, something went wrong
+      </div>
+      <Link href={'/'} className="p-3 border border-white rounded-2xl">
+          back to page
+      </Link>
+    </div>
+  )
 }
